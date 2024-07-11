6b6f8a47fbaf2f4a2aaffed455dad2b528499175
Yeonjae37 <jyj030307@gmail.com>
2024-07-12 01:31:06 +0900

    feat: BCrypt 암호화 메소드 추가

--- SpringSecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,6 +27,14 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+        http
+                .csrf((auth) -> auth.disable());
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
 
         return http.build();
     }
